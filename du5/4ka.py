a3474f14db63b2cf7f63d36d1bb9f3747c717b23
honzaik <honzaik@seznam.cz>
2019-11-10 13:49:53 +0100

    oprava nerovnosti

--- du5/4ka.py
+++ du5/4ka.py
@@ -2,45 +2,45 @@ import cvxpy as cp
 import numpy as np
 
 
-x1 = cp.Variable(5) #x+
-x2 = cp.Variable(5) #x
-t = cp.Variable(1)
-alfa = 9000
-A = np.matrix('-1.0 0.0 -3.0 0.0 5.0; -1.0 0.0 0.0 -1.0 2.0; 0.0, 0.0, 10.0, 0.0, -2.0; 3.0, -1.0, 0.0, 1.0, 0.0; -1.0, 1.0, -3.0, 1.0, -3.0');
-B = np.matrix('1.0 0.0 3.0 0.0 0.0; 1.0 0.0 0.0 1.0 0.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 1.0, 0.0, 0.0, 0.0; 1.0, 0.0, 3.0, 0.0, 3.0');
 
-obj = cp.Minimize(0)
-constraints = [x1>=0, x2>=1, A*x2 <= B*x1, x1-x2*alfa >= 0]
+def existsAtLeast(alpha):
+    A = np.matrix('-1.0 0.0 -3.0 0.0 5.0; -1.0 0.0 0.0 -1.0 2.0; 0.0, 0.0, 10.0, 0.0, -2.0; 3.0, -1.0, 0.0, 1.0, 0.0; -1.0, 1.0, -3.0, 1.0, -3.0')
+    B = np.matrix('1.0 0.0 3.0 0.0 0.0; 1.0 0.0 0.0 1.0 0.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 1.0, 0.0, 0.0, 0.0; 1.0, 0.0, 3.0, 0.0, 3.0')
+    x1 = cp.Variable(5) #x+
+    x2 = cp.Variable(5) #x
+    t = cp.Variable(1)
+    obj = cp.Minimize(0)
+    constraints = [x1>=0, x2>=0, A*x2 <= B*x1, x1-x2*alpha >= 0, sum(x2) == 1]
+    prob = cp.Problem(obj, constraints)
+    prob.solve()
+    print(x1.value)
+    print(x2.value)
+    print(prob.value)
+    if prob.value == np.inf:
+        return False
+    else:
+        print(min(x1.value/x2.value))
+        return True
 
-
-
-prob = cp.Problem(obj, constraints)
-prob.solve(max_iter=10000000)
-
-# Print result.
-print(prob.value)
-print(x1.value)
-print(x2.value)
-
-
-x1 = cp.Variable(5) #x+
-x2 = cp.Variable(5) #x
-t = cp.Variable(1)
-A = np.matrix('-1.0 0.0 -3.0 0.0 5.0; -1.0 0.0 0.0 -1.0 2.0; 0.0, 0.0, 10.0, 0.0, -2.0; 3.0, -1.0, 0.0, 1.0, 0.0; -1.0, 1.0, -3.0, 1.0, -3.0');
-B = np.matrix('1.0 0.0 3.0 0.0 0.0; 1.0 0.0 0.0 1.0 0.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 1.0, 0.0, 0.0, 0.0; 1.0, 0.0, 3.0, 0.0, 3.0');
-
-obj = cp.Maximize(t)
-constraints = [x1>=0, x2>=1, A*x2 <= B*x1, x1-x2*alfa == t, t >= -5000, t<= 5000]
-
-
-
-prob = cp.Problem(obj, constraints)
-prob.solve(max_iter=10000000)
-
-# Print result.
-print(prob.value)
-print(x1.value)
-print(x2.value)
-
+def existsLessThan(alpha):
+    A = np.matrix('-1.0 0.0 -3.0 0.0 5.0; -1.0 0.0 0.0 -1.0 2.0; 0.0, 0.0, 10.0, 0.0, -2.0; 3.0, -1.0, 0.0, 1.0, 0.0; -1.0, 1.0, -3.0, 1.0, -3.0')
+    B = np.matrix('1.0 0.0 3.0 0.0 0.0; 1.0 0.0 0.0 1.0 0.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 1.0, 0.0, 0.0, 0.0; 1.0, 0.0, 3.0, 0.0, 3.0')
+    x1 = cp.Variable(5) #x+
+    x2 = cp.Variable(5) #x
+    t = cp.Variable(1)
+    obj = cp.Minimize(0)
+    constraints = [x1>=0, x2>=0, A*x2 <= B*x1, x1-x2*alpha <= 0, sum(x2) == 1]
+    prob = cp.Problem(obj, constraints)
+    prob.solve()
+    print(x1.value)
+    print(x2.value)
+    print(prob.value)
+    if prob.value == np.inf:
+        return False
+    else:
+        print(min(x1.value/x2.value))
+        return True
 
 
+print(existsAtLeast(90))
+print(existsLessThan(30))
