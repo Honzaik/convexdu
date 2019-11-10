268cf3237dc30ab96ae4bc8555b5634734f0204e
Honzaik <honzaik@seznam.cz>
2019-11-10 17:39:50 +0100

    kod pro 4c

--- du5/4ka.py
+++ du5/4ka.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 def existsAtLeast(alpha):
-    A = np.matrix('-1.0, 0.0, -3.0, 0.0, 5.0; -1.0, 0.0, 0.0, -1.0, 2.0; 0.0, 0.0, 10.0, 0.0, -2.0; 3.0, -1.0, 0.0, 1.0, 0.0; -1.0, 1.0, -3.0, 1.0, -3.0')
     A = np.matrix('0, 0, 0, 0.0, 5.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 0.0, 10.0, 0.0, 0.0; 3.0, 0.0, 0.0, 1.0, 0.0; 0.0, 1.0, 0.0, 1.0, 0.0')
     B = np.matrix('1.0, 0.0, 3.0, 0.0, 0.0; 1.0, 0.0, 0.0, 1.0, 0.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 1.0, 0.0, 0.0, 0.0; 1.0, 0.0, 3.0, 0.0, 3.0')
     xplus = cp.Variable(5) #x+
@@ -14,34 +13,25 @@ def existsAtLeast(alpha):
     constraints = [x>=0, xplus>=0, B*xplus <= A*x, xplus-x*alpha >= 0, cp.sum(x) == 1]
     prob = cp.Problem(obj, constraints)
     prob.solve()
-    print(xplus.value)
-    print(x.value)
-    print(prob.value)
+    #print(xplus.value)
+    #print(x.value)
+    #print(prob.value)
     if prob.value == np.inf:
         return False
     else:
-        print(min(xplus.value/x.value))
+        #print(min(xplus.value/x.value))
         return True
 
-def existsLessThan(alpha):
-    A = np.matrix('-1.0 0.0 -3.0 0.0 5.0; -1.0 0.0 0.0 -1.0 2.0; 0.0, 0.0, 10.0, 0.0, -2.0; 3.0, -1.0, 0.0, 1.0, 0.0; -1.0, 1.0, -3.0, 1.0, -3.0')
-    B = np.matrix('1.0 0.0 3.0 0.0 0.0; 1.0 0.0 0.0 1.0 0.0; 0.0, 0.0, 0.0, 0.0, 2.0; 0.0, 1.0, 0.0, 0.0, 0.0; 1.0, 0.0, 3.0, 0.0, 3.0')
-    xplus = cp.Variable(5) #x+
-    x = cp.Variable(5) #x
-    t = cp.Variable(1)
-    obj = cp.Minimize(0)
-    constraints = [x>=0, xplus>=0, B*xplus <= A*x, xplus-x*alpha <= 0]
-    prob = cp.Problem(obj, constraints)
-    prob.solve()
-    print(xplus.value)
-    print(x.value)
-    print(prob.value)
-    if prob.value == np.inf:
-        return False
+leftBound = 0.01
+rightBound = 100.0
+
+while rightBound - leftBound > 0.005:
+    middle = (rightBound + leftBound)/2
+    if existsAtLeast(middle) == True:
+        leftBound = middle
     else:
-        print(min(xplus.value/x.value))
-        return True
+        rightBound = middle
 
-
-print(existsAtLeast(1.05))
-#print(existsLessThan(30))
+print(rightBound)
+print(leftBound)
+print('Opt. výsledek approx: ' + str((rightBound+leftBound)/2))
