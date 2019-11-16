17fc2c91d10ce31fdcc6aa4a06f04fec5e9d2ebb
honzaik <honzaik@seznam.cz>
2019-11-16 19:08:46 +0100

    du 6 done

--- du6/4ka.py
+++ du6/4ka.py
@@ -1,18 +1,16 @@
 import cvxpy as cp
 import numpy as np
 
+def calculateProfit(gamma):
+    x = cp.Variable(4)
+    profitVector = np.matrix('0.07, 0.08, 0.09, 0.1')
+    covarMatrix = np.matrix('1.1, -1.3, 2.5, -0.9; -1.3, 6.5, 0.7, -1.5; 2.5, 0.7, 11.1, 0.7; -0.9, -1.5, 0.7, 16.1')
+    obj = cp.Maximize((profitVector * x) - gamma*cp.quad_form(x,covarMatrix))
+    constraints = [cp.sum(x) == 1, x>=0]
+    prob = cp.Problem(obj, constraints)
+    prob.solve()
+    print(x.value)
 
-y = cp.Variable(7) #y_1 = log(v_1), y_2 = log(v_2), .., y_4 = log(v_4), y_5 = log(lambda), y_6 = log(c), y_7 = log(d)
-obj = cp.Minimize(cp.exp(y[4]))
-constraints = [y>=0,
-0.5*cp.exp(-y[4]-y[5])+0.7*cp.exp(-y[4]-y[5]-y[0]+y[1]+y[6])+0.5*cp.exp(-y[4]+y[5]-y[6]-y[0]+y[2])+0.4*cp.exp(-y[4]-y[0]+y[5]+y[6]+y[3])<=1,
-0.7*cp.exp(-y[4]-y[1]+y[0]) <= 1,
-0.7*cp.exp(-y[4]-y[2]+y[1]) <= 1,
-0.7*cp.exp(-y[4]-y[3]+y[2]) <= 1
-]
-prob = cp.Problem(obj, constraints)
-prob.solve()
-print(y.value)
-print(np.exp(y[4].value)) #lambda
-print(np.exp(y[5].value)) #c
-print(np.exp(y[6].value)) #d
+for gamma in np.arange(0,2.1,0.1):
+    calculateProfit(gamma)
+
