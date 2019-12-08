70e42fdfafa24b5c3e96672139255797f659df76
honzaik <honzaik@seznam.cz>
2019-12-08 15:22:04 +0100

    formatovani, 1,4 doneů

--- du9/4ka.py
+++ du9/4ka.py
@@ -21,31 +21,30 @@ prob.solve()
 # Print result.
 print("Vysledek obj funkce:", prob.value)
 print("P:", p.value)
-print("const :", constraints[0].dual_value)
-print("const :", constraints[1].dual_value)
-print("const :", constraints[2].dual_value)
-print("const :", constraints[3].dual_value)
-print("const :", constraints[4].dual_value)
-print("const :", constraints[5].dual_value)
-print("const :", constraints[6].dual_value)
-print("const :", constraints[7].dual_value)
-print("const :", constraints[8].dual_value)
+print("Q:")
+print("1:", constraints[2].dual_value)
+print("2:", constraints[3].dual_value)
+print("3:", constraints[4].dual_value)
+print("4:", constraints[5].dual_value)
+print("5:", constraints[6].dual_value)
+print("6:", constraints[7].dual_value)
+print("7:", constraints[8].dual_value)
 
 
-
+#pro kontrolu 
 ##worst case opt for P2 (Q)
 t = cp.Variable(1)
 p = cp.Variable(7)
 obj = cp.Maximize(t)
 constraints = [p >= 0, cp.sum(p)==1, 
-t <= -p[0]+p[1]+p[2]+p[3]-p[4]+p[5]+p[6],
-t <= p[0]-p[1]+p[2]+p[3]+p[4]-p[5]+p[6],
-t <= p[0]+p[1]-p[2]+p[3]+p[4]+p[5]-p[6],
-t <= p[0]+p[1]+p[2]+p[3]-p[4]-p[5]+p[6],
-t <= -p[0]-p[1]+p[2]+p[3]+p[4]+p[5]+p[6],
-t <= p[0]-p[1]-p[2]+p[3]+p[4]+p[5]+p[6],
-t <= p[0]+p[1]-p[2]-p[3]+p[4]+p[5]+p[6],
-t <= p[0]+p[1]+p[2]+p[3]+p[4]-p[5]-p[6],
+t <= -(-p[0]+p[1]+p[2]+p[3]-p[4]+p[5]+p[6]),
+t <= -(p[0]-p[1]+p[2]+p[3]+p[4]-p[5]+p[6]),
+t <= -(p[0]+p[1]-p[2]+p[3]+p[4]+p[5]-p[6]),
+t <= -(p[0]+p[1]+p[2]+p[3]-p[4]-p[5]+p[6]),
+t <= -(-p[0]-p[1]+p[2]+p[3]+p[4]+p[5]+p[6]),
+t <= -(p[0]-p[1]-p[2]+p[3]+p[4]+p[5]+p[6]),
+t <= -(p[0]+p[1]-p[2]-p[3]+p[4]+p[5]+p[6]),
+t <= -(p[0]+p[1]+p[2]+p[3]+p[4]-p[5]-p[6]),
 ]
 
 prob = cp.Problem(obj, constraints)
